86fd8af06966608d2319cc46c17c9cb55357cad6
gerswynlu <142557228+gerswynlu@users.noreply.github.com>
2024-05-27 14:04:17 +0800

    small edits

--- src/main/java/com/example/tigergrid/model/Form/Question.java
+++ src/main/java/com/example/tigergrid/model/Form/Question.java
@@ -18,13 +18,14 @@ public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String newQuestion;
     private String newInputType;
+
     @ElementCollection
     private List<String> newDropdownChoices;
+
     private int page;
-
-
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "form_id")
